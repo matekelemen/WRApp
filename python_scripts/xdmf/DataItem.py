f98cac167bf065faa4f92620c42d2831e2a0d573
matekelemen <kelemen.mate.12@gmail.com>
2024-04-01 11:17:38 +0200

    propagate Data shape through DataItem

--- python_scripts/xdmf/DataItem.py
+++ python_scripts/xdmf/DataItem.py
@@ -23,6 +23,11 @@ class DataItem(abc.ABC, Element):
         Element.__init__(self, "DataItem", {})
 
 
+    @abc.abstractmethod
+    def GetShape(self) -> "list[int]":
+        pass
+
+
 
 class LeafDataItem(DataItem):
 
@@ -31,6 +36,11 @@ class LeafDataItem(DataItem):
         self.attrib.update(data.GetAttributes())
         self.attrib["ItemType"] = "Uniform"
         self.text = data.GetText()
+        self.__shape = data.GetShape()
+
+
+    def GetShape(self) -> "list[int]":
+        return self.__shape
 
 
 
@@ -41,6 +51,11 @@ class UniformDataItem(DataItem):
         self.append(child)
         self.attrib.update(child.attrib)
         self.attrib["ItemType"] = "Uniform"
+
+
+    def GetShape(self) -> "list[int]":
+        child: DataItem = self[0]
+        return child.GetShape()
 
 
 
@@ -70,10 +85,22 @@ class CoordinateDataItem(DataItem):
 
         # The coordinate format restricts the size of its reference
         # data item.
-        index_shape = [int(d) for d in index_set.attrib["Dimensions"]]
-        reference_shape = [int(d) for d in index_set.attrib["Dimensions"]]
+        index_shape = index_set.GetShape()
+        reference_shape = reference_set.GetShape()
 
         if not all(i <= r for i, r in zip(index_shape, reference_shape)):
             raise RuntimeError(f"incompatible shapes (index set: {index_shape}, reference set {reference_shape})")
 
-        self.attrib["Dimensions"] = " ".join(str(d) for d in index_shape + reference_shape[len(index_shape):])
+        self.__shape: "list[int]" = index_shape + reference_shape[len(index_shape):]
+
+        # Remove 1s from the shape (otherwise paraview craps out sometimes)
+        shape = self.__shape.copy()
+        if shape:
+            shape = [v for v in shape if v != 1]
+            if not shape:
+                shape = [1]
+        self.attrib["Dimensions"] = " ".join(str(d) for d in shape)
+
+
+    def GetShape(self) -> "list[int]":
+        return self.__shape.copy()
