4675769cc808c8e802a3d63af2ea5b101d1320a2
matekelemen <kelemen.mate.12@gmail.com>
2024-04-04 21:40:51 +0200

    write indices instead of ids

--- packages/wrapp/utils/src/Hdf5IndexConnectivitiesOperation.cpp
+++ packages/wrapp/utils/src/Hdf5IndexConnectivitiesOperation.cpp
@@ -22,38 +22,30 @@ namespace Kratos::WRApp {
 
 
 namespace {
-HDF5::File::Vector<int> MakeIdMap(Ref<const HDF5::File::Vector<int>> rIds)
+void FillIdMap(Ref<const HDF5::File::Vector<int>> rIds,
+               Ref<HDF5::File::Vector<int>> rMap)
 {
-    HDF5::File::Vector<int> output;
-
-    // Find the highest ID and resize the output array accordingly
-    const std::size_t max_id = IndexPartition<std::size_t>(rIds.size()).for_each<MaxReduction<int>>(
-        [&rIds](std::size_t Index){return rIds[Index];}
-    );
-    output.resize(max_id + 1, false); // <== kratos IDs are 1-based
-    IndexPartition<std::size_t>(output.size()).for_each([&output](std::size_t Index) mutable {
-            output[Index] = 0;
-    });
-
     // Build the ID-index map
-    IndexPartition<std::size_t>(output.size()).for_each([&rIds, &output](std::size_t Index) mutable {
+    IndexPartition<std::size_t>(rMap.size()).for_each([&rIds, &rMap](std::size_t Index) mutable {
         if (Index < rIds.size()) {
-            output[rIds[Index]] = Index;
+            KRATOS_ERROR_IF_NOT(rIds[Index] < rMap.size()); // <== prevent segfaults
+            rMap[rIds[Index]] = Index;
         }
     });
-
-    return output;
 }
 
 void WriteSubGroupMaps(Ref<HDF5::File> rFile,
                        Ref<const std::string> rInputSubGroupPrefix,
                        Ref<const std::string> rOutputSubGroupPrefix,
                        Ref<const HDF5::File::Vector<int>> rNodeIdMap,
-                       Ref<const std::unordered_map<std::string,HDF5::File::Vector<int>>> rCellIdMaps)
+                       Ref<const HDF5::File::Vector<int>> rElementIdMap,
+                       Ref<const HDF5::File::Vector<int>> rConditionIdMap)
 {
     KRATOS_TRY
     const auto subgroup_names = rFile.GetGroupNames(rInputSubGroupPrefix);
     for (Ref<const std::string> r_subgroup_name : subgroup_names) {
+        if (r_subgroup_name == "Elements" || r_subgroup_name == "Conditions" || r_subgroup_name == "Properties") continue;
+
         const std::string input_subgroup_prefix = rInputSubGroupPrefix + "/" + r_subgroup_name;
         const std::string output_subgroup_prefix = rOutputSubGroupPrefix + "/" + r_subgroup_name;
 
@@ -61,10 +53,9 @@ void WriteSubGroupMaps(Ref<HDF5::File> rFile,
         {
             HDF5::File::Vector<int> node_ids;
 
+            // Read node IDs
             KRATOS_TRY
                 const std::string input_node_id_prefix = input_subgroup_prefix + "/" + "NodeIds";
-
-                // Read node IDs
                 const auto node_id_shape = rFile.GetDataDimensions(input_node_id_prefix);
                 KRATOS_ERROR_IF(node_id_shape.empty());
                 const std::size_t node_count = node_id_shape[0];
@@ -72,9 +63,14 @@ void WriteSubGroupMaps(Ref<HDF5::File> rFile,
                 rFile.ReadDataSet(input_node_id_prefix, node_ids, 0, node_ids.size());
             KRATOS_CATCH("")
 
+            // Map IDs to indices
+            IndexPartition<std::size_t>(node_ids.size()).for_each([&node_ids, &rNodeIdMap](std::size_t iNode){
+                node_ids[iNode] = rNodeIdMap[node_ids[iNode]];
+            });
+
             // Write node indices
             KRATOS_TRY
-                const std::string output_node_index_prefix = output_subgroup_prefix + "/Nodes/Ids";
+                const std::string output_node_index_prefix = output_subgroup_prefix + "/Nodes/Indices";
                 [[maybe_unused]] HDF5::WriteInfo write_info;
                 rFile.WriteDataSet(output_node_index_prefix,
                                    node_ids,
@@ -83,20 +79,17 @@ void WriteSubGroupMaps(Ref<HDF5::File> rFile,
         }
 
         // Write cell sets
-        for (std::string group_name : {"Elements", "Conditions"}) {
+        for (auto [group_name, p_id_map] : std::array<std::pair<std::string,Ptr<const HDF5::File::Vector<int>>>,2> {{{"Elements", &rElementIdMap}, {"Conditions", &rConditionIdMap}}}) {
             const std::string input_parent_prefix = input_subgroup_prefix + "/" + group_name;
+            Ref<const HDF5::File::Vector<int>> r_id_map = *p_id_map;
 
             if (rFile.HasPath(input_parent_prefix)) {
                 const std::string output_parent_prefix = output_subgroup_prefix + "/" + group_name;
                 const auto cell_names = rFile.GetGroupNames(input_parent_prefix);
                 for (const std::string& r_cell_name : cell_names) {
-                    const auto it_cell_id_map = rCellIdMaps.find(r_cell_name);
-                    KRATOS_ERROR_IF(it_cell_id_map == rCellIdMaps.end())
-                        << "cell type " << r_cell_name << " in subgroup is not in the root set";
-
-                    HDF5::File::Vector<int> cell_ids;
-
                     // Read cell IDs
+                    HDF5::File::Vector<int> cell_ids;
+                    HDF5::File::Vector<std::size_t> cell_indices;
                     KRATOS_TRY
                         const std::string cell_id_prefix = input_parent_prefix + "/" + r_cell_name + "/Ids";
                         const auto cell_id_shape = rFile.GetDataDimensions(cell_id_prefix);
@@ -106,30 +99,33 @@ void WriteSubGroupMaps(Ref<HDF5::File> rFile,
                         rFile.ReadDataSet(cell_id_prefix, cell_ids, 0, cell_ids.size());
                     KRATOS_CATCH("")
 
+                    // Map cell IDs to indices
+                    cell_indices.resize(cell_ids.size(), false);
+                    IndexPartition<std::size_t>(cell_ids.size()).for_each([&cell_ids, &cell_indices, &r_id_map](std::size_t iCell) {
+                        cell_indices[iCell] = r_id_map[cell_ids[iCell]];
+                    });
+
                     // Write cell indices to the HDF5 file
                     KRATOS_TRY
-                        const std::string output_cell_index_prefix = output_parent_prefix + "/" + r_cell_name + "/Ids";
+                        const std::string output_cell_index_prefix = output_parent_prefix + "/" + r_cell_name + "/Indices";
                         [[maybe_unused]] HDF5::WriteInfo write_info;
                         rFile.WriteDataSet(output_cell_index_prefix,
-                                           cell_ids,
+                                           cell_indices,
                                            write_info);
                     KRATOS_CATCH("")
                 } // for r_cell_name in cell_names
-            } // if input_parent_prefix in rFile
+
+            } // if r_subgroup_name in subgroup_names
         } // for group_name in ("Elements", "Conditions")
 
-        // Recursive call to nested subgroups
-        const std::string input_nesting_prefix = input_subgroup_prefix + "/SubModelParts";
-        if (rFile.HasPath(input_nesting_prefix)) {
-            KRATOS_TRY
-            const std::string output_nesting_prefix = output_subgroup_prefix + "/SubModelParts";
+        KRATOS_TRY
             WriteSubGroupMaps(rFile,
-                              input_nesting_prefix,
-                              output_nesting_prefix,
+                              input_subgroup_prefix,
+                              output_subgroup_prefix + "/SubModelParts",
                               rNodeIdMap,
-                              rCellIdMaps);
-            KRATOS_CATCH(input_nesting_prefix)
-        }
+                              rElementIdMap,
+                              rConditionIdMap);
+        KRATOS_CATCH(input_subgroup_prefix)
     } // for r_subgroup_names in subgroup_names
     KRATOS_CATCH("")
 }
@@ -232,39 +228,53 @@ void Hdf5IndexConnectivitiesOperation::Execute()
     {
         KRATOS_TRY
         const std::string input_node_id_prefix = mpImpl->mInputPrefix + "/Nodes/Local/Ids";
-        const std::string output_node_id_prefix = mpImpl->mOutputPrefix + "/Nodes/IdMap";
 
         const auto node_id_shape = p_file->GetDataDimensions(input_node_id_prefix);
         KRATOS_ERROR_IF(node_id_shape.empty());
         const std::size_t node_count = node_id_shape[0];
         HDF5::File::Vector<int> node_ids(node_count);
         p_file->ReadDataSet(input_node_id_prefix, node_ids, 0, node_ids.size());
-        node_id_map = MakeIdMap(node_ids);
 
-        [[maybe_unused]] HDF5::WriteInfo write_info;
-        p_file->WriteDataSet(output_node_id_prefix, node_id_map, write_info);
+        // Find the highest ID and resize the output array accordingly
+        const std::size_t max_id = IndexPartition<std::size_t>(node_ids.size()).for_each<MaxReduction<int>>(
+            [&node_ids](std::size_t Index){return node_ids[Index];}
+        );
+        node_id_map.resize(max_id + 1, false); // <== kratos IDs are 1-based
+
+        // Map IDs to indices
+        FillIdMap(node_ids, node_id_map);
         KRATOS_CATCH("nodes")
     }
 
     // Build and write the element and condition maps
-    std::unordered_map<
-        std::string,
-        HDF5::File::Vector<int>
-    > cell_id_maps;
+    HDF5::File::Vector<int> element_id_map, condition_id_map;
 
-    for (std::string parent_group_name : {"Elements", "Conditions"}) {
+    for (auto [parent_group_name, p_id_map] : std::array<std::pair<std::string,Ptr<HDF5::File::Vector<int>>>,2> {{{"Elements", &element_id_map}, {"Conditions", &condition_id_map}}}) {
         const std::string input_group_prefix = mpImpl->mInputPrefix + "/" + parent_group_name;
         const std::string output_group_prefix = mpImpl->mOutputPrefix + "/" + parent_group_name;
         const auto group_names = p_file->GetGroupNames(input_group_prefix);
 
+        // First, collect the total number of cells and resize the map accordingly.
+        // The assumption here is that each cell is appears exactly once in each
+        // group (every element has **one** unique type).
+        std::size_t cell_count = 0ul;
         for (const std::string& r_cell_group_name : group_names) {
-            const auto emplace_result = cell_id_maps.emplace(r_cell_group_name, HDF5::File::Vector<int> {});
-            KRATOS_ERROR_IF_NOT(emplace_result.second) << "duplicate cell type: " << r_cell_group_name;
-            Ref<HDF5::File::Vector<int>> r_cell_map = emplace_result.first->second;
+            KRATOS_TRY
+            const std::string input_cell_id_prefix = input_group_prefix + "/" + r_cell_group_name + "/Ids";
+            KRATOS_ERROR_IF_NOT(p_file->HasPath(input_cell_id_prefix));
+            KRATOS_ERROR_IF_NOT(p_file->IsDataSet(input_cell_id_prefix));
+            const auto cell_id_shape = p_file->GetDataDimensions(input_cell_id_prefix);
+            KRATOS_ERROR_IF(cell_id_shape.empty());
+            cell_count += cell_id_shape[0];
+            KRATOS_CATCH(r_cell_group_name)
+        }
+        p_id_map->resize(cell_count, false);
 
+        for (const std::string& r_cell_group_name : group_names) {
+            // Next, build the ID map.
+            // Each cell type has its own index space.
             KRATOS_TRY
                 const std::string input_cell_id_prefix = input_group_prefix + "/" + r_cell_group_name + "/Ids";
-                const std::string output_cell_id_prefix = output_group_prefix + "/" + r_cell_group_name + "/IdMap";
 
                 const auto cell_id_shape = p_file->GetDataDimensions(input_cell_id_prefix);
                 KRATOS_ERROR_IF(cell_id_shape.empty());
@@ -272,15 +282,18 @@ void Hdf5IndexConnectivitiesOperation::Execute()
                 HDF5::File::Vector<int> cell_ids(cell_count);
                 p_file->ReadDataSet(input_cell_id_prefix, cell_ids, 0, cell_ids.size());
 
-                r_cell_map = MakeIdMap(cell_ids);
-                [[maybe_unused]] HDF5::WriteInfo write_info;
-                p_file->WriteDataSet(output_cell_id_prefix, r_cell_map, write_info);
+                // Map IDs to indices
+                // @todo parallelize (@matekelemen)
+                std::size_t i_cell = 0ul;
+                for (auto cell_id : cell_ids) {
+                    p_id_map->operator[](cell_id) = i_cell++;
+                }
             KRATOS_CATCH(r_cell_group_name)
 
             HDF5::File::Matrix<int> index_connectivities;
 
-            // Read ID-based connectivities
             KRATOS_TRY
+                // Read ID-based connectivities
                 const std::string input_cell_connectivity_prefix = input_group_prefix + "/" + r_cell_group_name + "/Connectivities";
                 HDF5::File::Matrix<int> id_connectivities;
 
@@ -319,7 +332,8 @@ void Hdf5IndexConnectivitiesOperation::Execute()
                           input_nesting_prefix,
                           output_nesting_prefix,
                           node_id_map,
-                          cell_id_maps);
+                          element_id_map,
+                          condition_id_map);
         KRATOS_CATCH("")
     }
 
